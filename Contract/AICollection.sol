9df3c78e4ebfbb216f8311e11c1ea33b4fdb8d78
AI-NFTs <130778707+AI-NFTs@users.noreply.github.com>
2023-06-11 20:05:11 +0300

    Update AICollection.sol

--- Contract/AICollection.sol
+++ Contract/AICollection.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity >= 0.8.9;
 
 import "@openzeppelin/contracts@4.8.1/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts@4.8.1/token/ERC721/extensions/ERC721URIStorage.sol";
